698ff1b08fa4102a7c5506bf0e57cd15c2fc0b49
Lowzy2k <lacresia1@hotmail.com>
2023-12-21 01:44:19 +0200

    Components & Layouts & Constants

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const SITE_TITLE = 'TechPeeps Blog';
+export const SITE_DESCRIPTION = 'Articles, stories and tutorials from Tech Peeps';
+
+export const HOMEPAGE_ARTICLE_LIMIT = 7;
+export const ARTICLES_PER_PAGE =6;
